8ce043e6fd7c550c68533a508b6ec3cd7a646254
ahmad shami <ahmadshami48@gmail.com>
2023-04-06 23:58:57 +0300

    fixed bugs related to paths in LOCAL.sh

--- interact/account-helpers/LOCAL.sh
+++ interact/account-helpers/LOCAL.sh
@@ -8,8 +8,6 @@ cd $AXIOM_PATH
 echo "installing required installation scripts"
 git clone https://github.com/asempire/axiom-local.git
 
-cd AXIOM_PATH/axiom-local
-
 echo -e "Do you have a .csv with the appropriate configs? (y/N):"
 echo -n ">"
 read choice
@@ -21,16 +19,16 @@ then
 	if [[ ! -f $FILE_PATH ]];
 	then
 		echo "path provided doesn't exist and the csv creator file will be launched"
-		FILE_PATH="file_$(date +%s).csv"
-    python3 create_csv_file.py $FILE_PATH
+		FILE_PATH="$HOME/.axiom/axiom-local/file_$(date +%s).csv"
+    python3 $HOME/.axiom/axiom-local/create_csv_file.py $FILE_PATH
 	fi
 else
     echo "creating csv file"
-    FILE_PATH="file_$(date +%s).csv"
-    python3 create_csv_file.py $FILE_PATH
+    FILE_PATH="$HOME/.axiom/axiom-local/file_$(date +%s).csv"
+    python3 $HOME/.axiom/axiom-local/create_csv_file.py $FILE_PATH
 fi
 
 echo "The deployment process might take some time depending on the power of the machines and the internet connection so please be patient"
-bash -c "deploy.sh -f $FILE_PATH" #change this line when testing
+$HOME/.axiom/axiom-local/deploy.sh -f $FILE_PATH #change this line when testing
 echo "You can check the progress of the installation by running ~/.axiom/axiom-local/progress.sh"
 echo "you may need to restart the shell"
